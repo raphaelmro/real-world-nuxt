a2a1f3c09c5f7396d9423e298cd41f21836c7738
Raphael Onofre <raphaelmoura@codata.pb.gov.br>
2020-06-14 11:31:33 -0300

    Move api calls to service, create vuex event module, use vuex in events list, use vuex in event show

--- pages/index.vue
+++ pages/index.vue
@@ -11,19 +11,15 @@
   </div>
 </template>
 <script>
+import { mapState } from 'vuex'
 import EventCard from '@/components/EventCard.vue'
 export default {
-  head() {
-    return {
-      title: 'Event Listing'
-    }
+  components: {
+    EventCard
   },
-  async asyncData({ $axios, error }) {
+  async fetch({ store, error }) {
     try {
-      const { data } = await $axios.get('http://localhost:3000/events')
-      return {
-        events: data
-      }
+      await store.dispatch('events/fetchEvents')
     } catch (e) {
       error({
         statusCode: 503,
@@ -31,8 +27,13 @@ export default {
       })
     }
   },
-  components: {
-    EventCard
+  computed: mapState({
+    events: (state) => state.events.events
+  }),
+  head() {
+    return {
+      title: 'Event Listing'
+    }
   }
 }
 </script>
